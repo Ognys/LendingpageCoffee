32ff93d40926d9ef5297d80e6e84f110c2eea07e
Ogny <Myth4321@mail.ru>
2025-08-05 01:15:16 -0700

    Начата разработка адаптивности, добавлены некоторые медиа-запросы

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,22 @@
             header.classList.add("header-active");
         }
         else {
-            header.classList.remove("header-active")
+            header.classList.remove("header-active");
         }
     };
+}());
+
+// Burger
+
+(function () {
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".header-nav-menu");
+    const close = document.querySelector(".header-nav-close");
+    burger.addEventListener("click", () => {
+        menu.classList.add("header-nav-menu-active");
+    });
+        close.addEventListener("click", () => {
+        menu.classList.remove("header-nav-menu-active");
+    });
+
 }());
